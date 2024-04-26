41e482d0277ff0e34c5a0fa07bb420d474a5caa0
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-04-26 17:42:23 +0800

    wip docker compose

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,9 @@
+
+// module.exports = {
+//     url: 'mongodb://localhost:27017/lovvirRedemLogs'
+// };
+
 module.exports = {
-    url: 'mongodb://localhost:27017/lovvirRedemLogs'
+    url: 'mongodb://mongoDB:27017/lovvitRedemLogs'
 };
 
