54c8366f6f15dc54e3bcb09be265e6f6e971b9ad
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-04-26 18:00:39 +0800

    wip

--- config/database.config.js
+++ config/database.config.js
@@ -1,9 +1,4 @@
-
-// module.exports = {
-//     url: 'mongodb://localhost:27017/lovvirRedemLogs'
-// };
-
 module.exports = {
-    url: 'mongodb://mongoDB:27017/lovvitRedemLogs'
+    url: 'mongodb://localhost:27017/lovvirRedemLogs'
 };
 
