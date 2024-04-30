68d76df9e5c5238aceb279e82fc6ff62462504ec
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2024-04-30 17:06:01 +0800

    fix:docker-compose-tpl.yml

--- config/database.config.js
+++ config/database.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/lovvitRedemLogs'
-};
-
-// const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-
 // module.exports = {
-//   url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+//     url: 'mongodb://localhost:27017/lovvitRedemLogs'
 // };
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
+module.exports = {
+  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+};
 
